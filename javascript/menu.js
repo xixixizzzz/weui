a924c21b84c348475aa6fe4f583b243dd14fe0af
xixixizzzz <13731999@qq.com>
2017-11-25 15:44:00 +0800

    [add] refs #11301

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,5 +1,13 @@
 $(function() {
     $("#trade").on("click", function() {
-        window.location.href = "./trade.html"
+        window.location.href = "./trade.html";
+    });
+
+    $("#mywallet").on("click", function() {
+        window.location.href = "./mywallet.html";
+    });
+
+    $("#ledger").on("click", function() {
+        window.location.href = "./ledger.html";
     });
 });
